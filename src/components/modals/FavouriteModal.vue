5191a80f031de64e512ad5a00743ea91c22d92e0
Menilek Techane <menilek.techane@ultromics.com>
2022-04-23 15:24:23 +0100

    Add modal for viewing favourites

---
+++ src/components/modals/FavouriteModal.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="modal fade modal-text" id="favouriteModal" tabindex="-1" aria-labelledby="favouriteModalLabel"
+        aria-hidden="true" ref="modalElement">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title modal-text" id="favouriteModalLabel">Add favourites to see them here</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table">
+                        <thead>
+                            <th>#</th>
+                            <th>English</th>
+                            <th>Amharic</th>
+                        </thead>
+                        <tbody>
+                            <td>1</td>
+                            <td>Dog</td>
+                            <td>Wusha</td>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
