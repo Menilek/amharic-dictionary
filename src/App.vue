55636c5410637f91ad78d40dfc594fd6eeab2b00
Menilek Techane <menilek25@hotmail.co.uk>
2022-08-26 10:51:45 +0100

    Update loading fallback

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@ import Table from './components/Table.vue'
       <Table />
     </template>
     <template #fallback>
-      Loading...
+      <div class="loading-div">
+        <h1 id="big-font">ገና አልዚጊጅም</h1>
+        <h2>Gena alzigijum</h2>
+        <h3>Not ready yet</h3>
+      </div>
     </template>
   </Suspense>
 </template>
@@ -22,5 +26,25 @@ import Table from './components/Table.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.loading-div {
+  color: white;
+  padding-top: 15%;
+  transform: scale(1);
+  animation: pulse 1s linear infinite;
+}
+@keyframes pulse {
+  0% {
+    transform: scale(0.95);
+  }
+  70% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0.95);
+  }
+}
+#big-font {
+  font-size: 10vw;
 }
 </style>
